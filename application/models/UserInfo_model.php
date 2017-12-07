2136b761a359f16631f12d606f944c2ed5c2b1ae
SkinsArcade <skinsarcade@gmail.com>
2017-12-06 18:36:40 -1200

    renamed functions

--- application/models/UserInfo_model.php
+++ application/models/UserInfo_model.php
@@ -7,33 +7,35 @@ class UserInfo_model extends CI_Model {
 		parent::__construct();
 		$this->load->library( 'Geolocation' );
 		$this->load->config( 'geolocation', true );
-		$this->load->helper('url');
-		$this->load->library('user_agent');
+		$this->load->helper( 'url' );
+		$this->load->library( 'user_agent' );
 	}
 
-	public function geolocation() {
+	public function get_ip() {
+
+		return $this->input->ip_address();
+	}
+
+	public function get_geolocation() {
 		$config = $this->config->config['geolocation'];
-		$ip     = $this->input->ip_address();
+		$ip     = $this->get_ip();
 		$this->geolocation->initialize( $config );
 		$this->geolocation->set_ip_address( $ip ); // IP to locate
 		$this->geolocation->set_format( 'json' );
 		// OR you can change the format within `config/geolocation.php` config file
 		$result = json_decode( $this->geolocation->get_city(), true );
 
-		$location_address = $result['cityName'] . ', ' .
-		                    $result['regionName'] . ', ' .
-		                    $result['countryName'];
-
-		$data['ip'] = $result['ipAddress'];
-		$data['location'] = $location_address;
+		$data = $result['cityName'] . ', ' .
+		        $result['regionName'] . ', ' .
+		        $result['countryName'];
 
 		return $data;
 	}
 
-	public function browser() {
-		$data['browser'] = $this->agent->browser();
+	public function get_browser() {
+		$data['browser']        = $this->agent->browser();
 		$data['browserVersion'] = $this->agent->version();
-		$data['platform'] = $this->agent->platform();
+		$data['platform']       = $this->agent->platform();
 //		$data['full_user_agent_string'] = $_SERVER['HTTP_USER_AGENT'];
 
 		return $data;
